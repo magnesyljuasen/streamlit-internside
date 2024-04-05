7f5682b410b4f16c3abb7f3deaf64e0a30dd09d7
magnesyljuasen <msylju@gmail.com>
2024-04-05 15:01:43 +0200

    oppdatert med link ernstromgruppen

--- Forside.py
+++ Forside.py
@@ -86,6 +86,7 @@ with open("styles/main.css") as f:
             st.write("[• Dashboard Melhus](%s)" % "https://demo-melhus.streamlit.app/")
             st.write("[• Dashboard Kolbotn](%s)" % "https://demo-kolbotn.streamlit.app/")
             st.write("[• Dashboard Røa](%s)" % "https://datalogging-roa.streamlit.app/")
+            st.write("[• Geotermos Ernströmgruppen](%s)" % "https://www.geotermos.no/")
     with tab3:
         c1, c2 = st.columns(2)
         with c1:
